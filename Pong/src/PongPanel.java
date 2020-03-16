b0210df67b3961a7b9baf1604d9f6cf0ab7d3925
codeywgithub <codey.atkinson@gmail.com>
2020-03-16 14:35:00 +1100

    Added rectangle and Timer to JPanel

--- Pong/src/PongPanel.java
+++ Pong/src/PongPanel.java
@@ -4,14 +4,32 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 public class PongPanel extends JPanel implements ActionListener, KeyListener {
 
 	private final static Color BACKGROUND_COLOUR = Color.black;
+	private final static int TIMER_DELAY = 5;
 	
 	public PongPanel() {
 		setBackground(BACKGROUND_COLOUR);
+		Timer timer = new Timer(TIMER_DELAY, this);
+		timer.restart();
 	}
+	
+	private void update() {
+		
+	}
+	
+	
+	@Override
+	 public void paintComponent(Graphics g) {
+	     super.paintComponent(g);
+	     g.setColor(Color.WHITE);
+	     g.fillRect(20, 20, 100, 100);
+	 }
 	
 	
 	@Override
@@ -35,6 +53,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		update();
 		
 	}
 
