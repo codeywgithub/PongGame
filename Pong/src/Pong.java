e23886ef8577f404e01e8836963942acca4e1e67
codeywgithub <codey.atkinson@gmail.com>
2020-03-16 14:06:26 +1100

    16/03/2020 Added background color of black and added new object aid

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,15 +1,23 @@
 import javax.swing.JFrame;
 
-
 public class Pong extends JFrame {
 	
+	private final static String WINDOW_TITLE = "Pong";
+	private final static int WINDOW_WIDTH = 800;
+	private final static int WINDOW_HEIGHT = 600;
+	
+	
+	
 	public Pong() {
-		setTitle("Pong");
-		setSize(800, 600);
-		setResizable(true);
+		add(new PongPanel());
+		setTitle(WINDOW_TITLE);
+		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		setResizable(false);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
 		//Opens up visual window in the above specs
+		
 	}
 
 	public static void main(String[] args) {
@@ -17,7 +25,6 @@ public class Pong extends JFrame {
 
 		new Pong();
 		//seems to initialize the above specs window with main method.
-		
 		
 	}
 
